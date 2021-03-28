48c00f0d90c944d62f5d9b0f1eb80e31b40bcd95
JackSon_tm.m <RyanTokManMokMTM@hotmail.com>
2021-03-28 13:17:23 +0800

    Working on 3rd Login: Goole Login is now working

---
+++ IOS_DEV/IOS_DEV/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  SwiftUIView.swift
+//  new
+//
+//  Created by 張馨予 on 2021/1/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var message:String = ""
+    var body: some View {
+        Text(message)
+    }
+}
+ 
+ 
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            
+    }
+}
+
